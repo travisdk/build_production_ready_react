d384e56da285eec812cb2cd62615b8cfe4d9a94b
Henrik Juhl (hjemme) <henrik.juhl_dk@hotmail.com>
2023-04-25 14:05:51 +0200

    upload form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 // COMPONENTS
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 
 // CSS
 import "./App.css";
+
+// TEST DATA
 const photos = [
   "https://picsum.photos/id/1001/200/200",
   "https://picsum.photos/id/1002/200/200",
@@ -14,14 +18,30 @@ const photos = [
 ];
 
 function App() {
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapse] = useState(false);
+
+  const toggle = () => {
+    collapse(!isCollapsed);
+  };
+
   return (
     <>
       <div className="container text-center mt-5">
+        <button className="btn btn-warning" onClick={() => setItems(["https://picsum.photos/id/1005/200/200", ...items])}>
+          ADD
+        </button>
+        <button className="btn btn-success mx-2" onClick={toggle}>
+          collapse
+        </button>
+
         <h1>Gallery</h1>
+
         <Navbar />
+
         <div className="row">
-          {photos.map((photo) => {
-            return <Card src={photo} />;
+          {items.map((photo, index) => {
+            return <Card key={index} src={photo} />;
           })}
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,57 @@
+const Navigation = () => {
+  return (
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      {/* remove all links except HOME */}
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="#">
+          Home
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+const SearchForm = () => {
+  return (
+    <form className="d-flex">
+      <input
+        className="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <button className="btn btn-outline-success" type="submit">
+        Search
+      </button>
+    </form>
+  );
+};
+const DropDown = () => {
+  return (
+    <ul className="navbar-nav mb-2 mb-lg-0">
+      {/* remove ms-auto */}
+      <li className="nav-item dropdown">
+        <a
+          className="nav-link dropdown-toggle"
+          href="#"
+          id="navbarDropdown"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Login
+        </a>
+        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li>
+            <a className="dropdown-item text-center" href="#">
+              Profile
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  );
+};
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
@@ -17,48 +71,9 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {/* remove all links except HOME */}
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-          <ul className="navbar-nav mb-2 mb-lg-0">
-            {" "}
-            {/* remove ms-auto */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Login
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="dropdown-item text-center" href="#">
-                    Profile
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
+          <Navigation />
+          <SearchForm />
+          <DropDown />
         </div>
       </div>
     </nav>
