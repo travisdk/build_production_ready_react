01f8facd7038d83db7df4fddc0b329d38a9459c9
Henrik Juhl (hjemme) <henrik.juhl_dk@hotmail.com>
2023-05-04 17:51:42 +0200

    routing added

---
+++ src/components/StockImages.js
@@ -0,0 +1,25 @@
+import List from "./List";
+import { useFirestoreContext } from "../context/FirestoreContext";
+import { useMemo } from "react";
+import { useAuthContext } from "../context/AuthContext";
+
+const StockImages = () => {
+  const { state } = useFirestoreContext();
+  const { currentUser } = useAuthContext();
+  const username = currentUser?.displayName.toLowerCase().split(" ").join("");
+
+  const items = useMemo(() => {
+    const filtered = state.items.filter((item) => {
+      return item.user === username;
+    });
+    return currentUser ? filtered : [];
+  }, [currentUser, state, username]);
+  return (
+    <>
+      <h1> My Stock Images</h1>
+      <List items={items} />
+    </>
+  );
+};
+
+export default StockImages;
